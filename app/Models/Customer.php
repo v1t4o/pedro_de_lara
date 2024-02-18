791a50a9e3746eaf9ae3526b577c09020325ebc7
Victor de Oliveira <vitor.oliveira.marinho@gmail.com>
2024-02-17 22:44:51 -0300

    feat: added relation between Customer and Transaction models

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Customer extends Model
 {
   //
+  public function transactions(): HasMany
+  {
+    return $this->hasMany(Transaction::class);
+  }
 }
